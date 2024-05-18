841d69f12d72c2a2571f6f61927ed5307dc5d474
baohc <baohc110902@gmail.com>
2024-05-19 03:15:19 +0700

    chore: Add Google OAuth2 configuration and dependencies

--- src/services/authen.service.js
+++ src/services/authen.service.js
@@ -1,6 +1,25 @@
 'use strict'
 const prisma = require('../configs/prisma.config').client
 
-class AuthenService {}
+class AuthenService {
+    loginStrategies = {
+        email: this.loginWithEmail,
+        google: this.loginWithGoogle,
+    }
+
+    static login = async (user, typeLogin) => {
+        return await this.loginStrategies[typeLogin](user)
+    }
+
+    loginWithGoogle = async (user) => {
+        // return await AuthenService.login(user, 'google')
+        return 'google'
+    }
+
+    loginWithPhone = async (user) => {
+        // return await AuthenService.login(user, 'phone')
+        return 'phone'
+    }
+}
 
 module.exports = AuthenService
